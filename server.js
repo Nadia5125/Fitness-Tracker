466f43b6212704a9fa019e02c437e714a525791c
Nadia <nadiamiswiyati@gmail.com>
2020-03-25 12:35:51 -0400

    Fitness Tracker

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
+var PORT = process.env.PORT || 2030;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controller/fitness_controller");
+app.use(routes);
+
+//TESTING!!
+// var URI = process.env.MONGODB_URI || "mongodb://user:nadia2020@ds151997.mlab.com:51997/heroku_rwh6btjv";
+
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
